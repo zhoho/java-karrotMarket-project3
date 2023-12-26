d0f4191e1568e688b16faacd99d5043c23c03b04
Zho <chlwlgh1011@naver.com>
2023-12-26 15:29:48 +0900

    feat: Item 상품 getter,setter 생성

---
+++ src/main/java/com/example/Item.java
@@ -0,0 +1,47 @@
+package com.example;
+
+public class Item {
+    private int id;
+    private String seller;
+    private int price;
+    private String itemName;
+
+    public Item(int id, String seller, String itemName, int price) {
+        this.id = id;
+        this.seller = seller;
+        this.price = price;
+        this.itemName = itemName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
