76c1f7662e6ceafc6495358fced2ef8d35766d3e
Zho <chlwlgh1011@naver.com>
2023-12-26 16:02:18 +0900

    feat: 종료 선택 전까지 메뉴 선택 반복 되도록 구현

--- src/main/java/com/example/controller/KarrotMarketController.java
+++ src/main/java/com/example/controller/KarrotMarketController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.Constant.constant;
 import com.example.Create;
 import com.example.view.InputView;
 import com.example.view.OutputView;
@@ -10,31 +11,35 @@ public class KarrotMarketController {
     }
 
     public void karrotFlow() {
-        int selectedMenu = InputView.selectMenu();
-        menuRouter(selectedMenu);
+        while(true) {
+            int selectedMenu = InputView.selectMenu();
+
+            if(selectedMenu == constant.ZERO) {
+                System.out.println("종료하기");
+                break;
+            }
+            menuRouter(selectedMenu);
+        }
     }
 
     public void menuRouter(int selectedMenu) {
-        if(selectedMenu == 1) {
-            System.out.println("조회하기");
-        }
-        else if(selectedMenu == 2) {
-            Create.addItem();
-        }
-        else if(selectedMenu == 3) {
-            System.out.println("수정하기");
-        }
-        else if(selectedMenu == 4) {
-            System.out.println("삭제하기");
-        }
-        else if(selectedMenu == 5) {
-            System.out.println("검색하기");
-        }
-        else if(selectedMenu == 0) {
-            System.out.println("종료하기");
-        }
-        else {
-            throw new IllegalArgumentException("[ERROR] 잘못된 입력");
-        }
+            if(selectedMenu == constant.ONE) {
+                System.out.println("조회하기");
+            }
+            else if(selectedMenu == constant.TWO) {
+                Create.addItem();
+            }
+            else if(selectedMenu == constant.THREE) {
+                System.out.println("수정하기");
+            }
+            else if(selectedMenu == constant.FOUR) {
+                System.out.println("삭제하기");
+            }
+            else if(selectedMenu == constant.FIVE) {
+                System.out.println("검색하기");
+            }
+            else {
+                throw new IllegalArgumentException("[ERROR] 잘못된 입력");
+            }
     }
 }
