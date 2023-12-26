e05deea2acf0aee6209a1f0d029ea2f7a93023b1
Zho <chlwlgh1011@naver.com>
2023-12-26 16:01:27 +0900

    feat: constant 생성

---
+++ src/main/java/com/example/Constant/constant.java
@@ -0,0 +1,10 @@
+package com.example.Constant;
+
+public class constant {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+}
