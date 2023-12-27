0e404c96234d4aa366427d368b68ff10382849bb
Zho <chlwlgh1011@naver.com>
2023-12-27 15:39:26 +0900

    feat:constant 분리

--- src/main/java/com/example/Constant/constant.java
+++ src/main/java/com/example/Constant/constant.java
@@ -7,4 +7,10 @@ public class constant {
     public static final int THREE = 3;
     public static final int FOUR = 4;
     public static final int FIVE = 5;
+//    public static final String MENUSHOWALL = "상품 전체 보기";
+//    public static final String MENUADD = "상품 추가";
+//    public static final String MENUEDIT = "상품 수정";
+//    public static final String MENUDELETE = "상품 등록 취소";
+//    public static final String MENUSEARCH = "상품 이름 검색";
+    public static final String MENUEXIT = "종료";
 }
