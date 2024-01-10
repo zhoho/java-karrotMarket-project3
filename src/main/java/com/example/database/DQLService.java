4161453fc721f0f633f229cf0442e37d095d336b
Zho <chlwlgh1011@naver.com>
2024-01-10 20:15:12 +0900

    feat: 상품명으로 검색 기능 구현

--- src/main/java/com/example/database/DQLService.java
+++ src/main/java/com/example/database/DQLService.java
@@ -2,20 +2,19 @@ package com.example.database;
 
 import com.example.Item;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static com.example.Item.items;
 
 public class DQLService {
     static final String SELECT_ALL = "select * from karrotItem ";
-    static PreparedStatement pstmt;
+    static final String SELECT_INCLUDE_NAME = "SELECT * FROM karrotItem WHERE itemName LIKE '%' || ? || '%' ORDER BY id";
+
 
     public static void getAllData() {
         items.clear();
         try(Connection conn = DBConnection.getConnection()) {
+            PreparedStatement pstmt;
             pstmt = conn.prepareStatement(SELECT_ALL);
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
@@ -31,4 +30,24 @@ public class DQLService {
         }
     }
 
-}
+    public static void getDataIncludeItemName(String inputItemName) {
+        items.clear();
+        try (Connection conn = DBConnection.getConnection()){
+            PreparedStatement pstmt;
+            pstmt = conn.prepareStatement(SELECT_INCLUDE_NAME);
+            pstmt.setString(1, inputItemName);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String seller = rs.getString("seller");
+                String itemName = rs.getString("itemName");
+                int price = rs.getInt("price");
+                String dateTime = rs.getString("dateTime");
+                items.add(new Item(id, seller,itemName,price,dateTime));
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+     }
+    }
